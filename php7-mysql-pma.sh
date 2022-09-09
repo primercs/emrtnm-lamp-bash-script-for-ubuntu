84b046882c55bb9a5c62a90795d5b174c0fa1795
Primercs <35300052+primercs@users.noreply.github.com>
2022-09-09 10:05:10 -0300

    Update php7-mysql-pma.sh

--- php7-mysql-pma.sh
+++ php7-mysql-pma.sh
@@ -23,7 +23,7 @@ sudo echo "<?php phpinfo();" >> /var/www/html/index.php
 printf $DIVIDER
 printf "PHP7.4 is installing..."
 printf $DIVIDER
-
+sudo add-apt-repository -y ppa:ondrej/php && sudo apt-get update
 sudo apt-get install php7.4 libapache2-mod-php7.4 php7.4-cli php7.4-gd php7.4-mysql php7.4-imap php7.4-curl php7.4-intl php7.4-pspell php7.4-sqlite3 php7.4-tidy php7.4-xmlrpc php7.4-xsl php7.4-zip php7.4-mbstring php7.4-soap php7.4-opcache libicu65 php7.4-common php7.4-json php7.4-readline php7.4-xml -y
 
 printf $DIVIDER
